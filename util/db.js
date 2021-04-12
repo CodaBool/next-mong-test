b5b50d689aaf255a47ab7949f9c5411b7ae17d58
CodaBool <CodaBool@pm.me>
2021-04-12 18:55:44 -0400

    more log

--- util/db.js
+++ util/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 export default async () => {
-  console.log('ENV =', process.env.MONGODB_URI)
+  console.log('TEST_ENV =', process.env.MONGODB_URI)
+  console.log('MONGODB_URI =', process.env.MONGODB_URI)
   if (mongoose.connection.readyState >= 1) return
   return mongoose.connect(
     process.env.MONGODB_URI,
