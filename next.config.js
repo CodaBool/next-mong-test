c15752359f8bd3bff96021f6995ee193cf138c38
CodaBool <CodaBool@pm.me>
2021-04-13 17:07:51 -0400

    send down pipe

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  target: 'serverless',
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    TEST_ENV: process.env.TEST_ENV,
-    STAGE: process.env.STAGE
-  }
-};
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,15 @@ import axios from 'axios'
 
 export default function index() {
 
+  console.log('var', )
+
   function handleClick() {
-    console.log('click')
     axios.get('/api/test')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data))
+  }
+  function handleSimpleClick() {
+    axios.get('/api/simpleTest')
       .then(res => console.log(res.data))
       .catch(err => console.log(err.response.data))
   }
@@ -13,7 +19,10 @@ export default function index() {
   return (
     <div>
       <h1>Home</h1>
+      <h1>STAGE = {process.env.NEXT_PUBLIC_STAGE}</h1>
+      <h1>URI = {process.env.MONGODB_URI}</h1>
       <button onClick={handleClick}>Click</button>
+      <button onClick={handleSimpleClick}>handleSimpleClick</button>
     </div>
   )
 }
