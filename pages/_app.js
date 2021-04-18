eb23f451837ba7c3239244e6e2ce54fc7ef6f233
Coda Bool <codabool@pm.me>
2021-04-18 03:46:23 -0400

    website sls test

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,9 @@ export default function app({ Component, pageProps }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Navigation />
       <main>
-        <Container>
           <Component {...pageProps} />
-        </Container>
       </main>
-      <Footer />
     </div>
   )
 }
